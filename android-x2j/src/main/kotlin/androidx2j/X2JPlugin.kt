a214f4a76237b57ad4c21d2665810a9e54516a36
huangzhenzi <huangzhenzi@sensetime.com>
2022-04-02 23:45:57 +0800

    refactor: X2JPlugin

--- android-x2j/src/main/kotlin/androidx2j/X2JPlugin.kt
+++ android-x2j/src/main/kotlin/androidx2j/X2JPlugin.kt
@@ -1,13 +1,15 @@
 package androidx2j
 
 import com.android.build.gradle.AppExtension
+import com.android.build.gradle.BaseExtension
 import com.android.build.gradle.LibraryExtension
 import com.android.build.gradle.api.BaseVariant
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import java.io.File
+import java.util.*
 
-@Suppress("UnstableApiUsage")
+@Suppress("UnstableApiUsage", "DefaultLocale")
 class X2JPlugin : Plugin<Project> {
     private val layoutFiles = hashMapOf<String, File>()
     private var isAndroidLibrary = false
@@ -22,32 +24,16 @@ class X2JPlugin : Plugin<Project> {
                 return
             }
             isAndroidLibrary = androidLib != null
-
-            android.sourceSets.getByName("main").res.srcDirs.asSequence()
-                .flatMap { it.listFiles()?.asSequence() ?: emptySequence() }
-                .filter { it.name.startsWith("layout") }
-                .flatMap { it.listFiles()?.asSequence() ?: emptySequence() }
-                .forEach { layoutFiles[it.nameWithoutExtension] = it }
-
-            val hasKaptPlugin = project.pluginManager.hasPlugin("kotlin-kapt")
-            val apt = if (hasKaptPlugin) "kapt" else "annotationProcessor"
-            val x2cVersion = "-SNAPSHOT"
-            project.configurations.getByName(apt).dependencies
-                .add(project.dependencies.create("com.github.7hens.X2C:x2c-apt:$x2cVersion"))
-            project.configurations.getByName("implementation").dependencies
-                .add(project.dependencies.create("com.github.7hens.X2C:x2c-lib:$x2cVersion"))
-            if (android.dataBinding.isEnabled) {
-                project.configurations.getByName("implementation").dependencies
-                    .add(project.dependencies.create("com.github.7hens.X2C:x2c-binding:$x2cVersion"))
-            }
-
+            findLayoutFiles(android)
+            implementationX2JDependencies(project, android)
             android.registerTransform(X2JTransform(android))
 
             val outputDir = File(project.buildDir, "generated/source/x2j/main")
             android.sourceSets.findByName("main")!!.java.srcDir(outputDir)
             project.afterEvaluate {
-                (androidApp?.applicationVariants ?: androidLib?.libraryVariants)?.forEach {
-                    generateX2J(project, outputDir, it)
+                (androidApp?.applicationVariants ?: androidLib?.libraryVariants)?.forEach { variant ->
+                    generateX2JFile(project, outputDir, variant)
+                    generateRFile(project, outputDir, variant)
                 }
             }
         } catch (e: Throwable) {
@@ -55,13 +41,33 @@ class X2JPlugin : Plugin<Project> {
         }
     }
 
-    private fun generateX2J(project: Project, outputRootDir: File, variant: BaseVariant) {
-        val variantName = variant.name
-        val yaVariantName = variantName.capitalize()
-        val applicationId = variant.applicationId
-        val sep = File.separator
+    private fun findLayoutFiles(android: BaseExtension) {
+        android.sourceSets.getByName("main").res.srcDirs.asSequence()
+            .flatMap { it.listFiles()?.asSequence() ?: emptySequence() }
+            .filter { it.name.startsWith("layout") }
+            .flatMap { it.listFiles()?.asSequence() ?: emptySequence() }
+            .forEach { layoutFiles[it.nameWithoutExtension] = it }
+    }
 
-        val genX2JTask = project.tasks.create("generate${yaVariantName}X2J") {
+    private fun implementationX2JDependencies(project: Project, android: BaseExtension) {
+        val hasKaptPlugin = project.pluginManager.hasPlugin("kotlin-kapt")
+        val apt = if (hasKaptPlugin) "kapt" else "annotationProcessor"
+        val x2cVersion = "-SNAPSHOT"
+        project.configurations.getByName(apt).dependencies
+            .add(project.dependencies.create("com.github.7hens.X2C:x2c-apt:$x2cVersion"))
+        project.configurations.getByName("implementation").dependencies
+            .add(project.dependencies.create("com.github.7hens.X2C:x2c-lib:$x2cVersion"))
+        if (android.dataBinding.isEnabled) {
+            project.configurations.getByName("implementation").dependencies
+                .add(project.dependencies.create("com.github.7hens.X2C:x2c-binding:$x2cVersion"))
+        }
+    }
+
+    private fun generateX2JFile(project: Project, outputRootDir: File, variant: BaseVariant) {
+        val variantName = variant.name.capitalize()
+        val applicationId = variant.applicationId
+
+        val genX2JTask = project.tasks.create("generate${variantName}X2J") {
             group = "build"
             doLast {
                 MyLogger.log("generate X2J file")
@@ -80,19 +86,25 @@ class X2JPlugin : Plugin<Project> {
             }
         }
         variant.registerJavaGeneratingTask(genX2JTask, outputRootDir)
+    }
 
+    private fun generateRFile(project: Project, outputRootDir: File, variant: BaseVariant) {
+        val variantName = variant.name.capitalize()
+        val applicationId = variant.applicationId
+
+        val sep = File.separator
         val rFilePath = applicationId.replace(".", sep) + sep + "R.java"
         val x2jRFile = File(outputRootDir, rFilePath.replace("R.java", "X2J_R.java"))
         val x2jRGenerator = X2JRFileGenerator(applicationId, x2jRFile)
 
         if (isAndroidLibrary) {
-            project.tasks.getByName("generate${yaVariantName}RFile").doLast {
+            project.tasks.getByName("generate${variantName}RFile").doLast {
                 MyLogger.log("generate X2J_R.java file")
                 val rTxtFile = outputs.files.files.first { it.absolutePath.endsWith("R.txt") }
                 x2jRGenerator.fromRTxt(rTxtFile)
             }
         } else {
-            project.tasks.getByName("process${yaVariantName}Resources").doLast {
+            project.tasks.getByName("process${variantName}Resources").doLast {
                 MyLogger.log("generate X2J_R.java file")
                 outputs.files.files.onEach { MyLogger.log(it) }
                 val rJavaDir = outputs.files.files.firstOrNull {
